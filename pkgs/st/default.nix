b07aa4028f51e534450bb057a0ba5361571baace
Michael Adler <therisen06@gmail.com>
2021-05-26 20:58:34 +0200

    st: fix umlaute

--- pkgs/st/default.nix
+++ pkgs/st/default.nix
@@ -1,13 +1,24 @@
-{ sources, pkgs, fetchFromGitHub, harfbuzz }:
+{ sources, pkgs, fetchFromGitHub, harfbuzz, glibcLocales, makeWrapper }:
 
 pkgs.st.overrideAttrs (old: {
 
   buildInputs = old.buildInputs ++ [ harfbuzz ];
 
+  nativeBuildInputs = old.nativeBuildInputs ++ [ makeWrapper ];
+
   src = fetchFromGitHub (with sources.st; { inherit owner repo rev sha256; });
+
+  postPatch = ''
+    # skip terminfo which conflicts with ncurses
+    sed -i '/tic /d' Makefile
+  '';
 
   preInstall = ''
     sed -i -e 's/git submodule .*/# skipped/' Makefile
+  '';
+
+  postInstall = ''
+    wrapProgram "$out/bin/st" --set LOCALE_ARCHIVE ${glibcLocales}/lib/locale/locale-archive
   '';
 
 })

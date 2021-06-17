c8e8e1e7403b2f7d995d9d8b531b94c91cae43c1
Michael Adler <therisen06@gmail.com>
2021-06-17 10:04:25 +0200

    Remove dwm patches

--- pkgs/dwm/default.nix
+++ pkgs/dwm/default.nix
@@ -10,16 +10,4 @@ prev.dwm.overrideAttrs (old: {
     sha256 = "1dazcwm3l9nnx1h6yv4snr8q4v548b7ri6lly1s7f55nwjwzbyni";
   };
 
-  patches = (prev.dwm.patches or [ ]) ++ [
-    ./patches/0001-cyclelayout.patch
-    ./patches/0002-Attach-bottom.patch
-    ./patches/0003-Disable-focus-follows-mouse.patch
-    ./patches/0004-Put-master-to-exact-position-of-zoomed-client.patch
-    ./patches/0005-pertag-keep-layout-mwfact-barpos-and-nmaster-per-tag.patch
-    ./patches/0006-fancybar-show-the-titles-of-all-visible-windows.patch
-    ./patches/0007-taglabels.patch
-    ./patches/0008-Cool-autostart.patch
-    ./patches/0009-save-floats-patch.patch
-  ];
-
 })

34228385d9b0a6240da0a5222594349bda082897
Michael Adler <therisen06@gmail.com>
2021-07-08 11:18:02 +0200

    Proper error handling in subshells

--- .ci/update-all.sh
+++ .ci/update-all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -p bash -p git -p nixUnstable -p update-nix-fetchgit -i bash
+#!nix-shell -p bash -p git -p nixUnstable -p update-nix-fetchgit -i "bash -euET -o pipefail -O inherit_errexit"
 # shellcheck shell=bash
-set -eux -o pipefail
+set -x
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 
 mkdir -p ~/.config/nix
@@ -10,17 +10,19 @@ printf "access-tokens = github.com=%s" "$GITHUB_TOKEN" >>~/.config/nix/nix.conf
 chmod 600 ~/.config/nix/nix.conf
 
 ROOT_DIR=$(readlink -f "${DIR}/..")
-cd "$ROOT_DIR"
+cd "$ROOT_DIR" || exit 1
 
 # flakes
-nix --experimental-features "nix-command flakes" flake update
+(nix --experimental-features "nix-command flakes" flake update) &
 
 # firefox and friends
-nix run '.#firefox-addons-generator' -- pkgs/firefox-addons/addons.json pkgs/firefox-addons/generated-addons.nix
-pkgs/firefox-bin/update.sh
+(nix run '.#firefox-addons-generator' -- pkgs/firefox-addons/addons.json pkgs/firefox-addons/generated-addons.nix) &
+(pkgs/firefox-bin/update.sh) &
 
 # everything else
-find pkgs -name "*.nix" -not -path "pkgs/zig/*" -print0 | xargs -0 update-nix-fetchgit
+(find pkgs -name "*.nix" -not -path "pkgs/zig/*" -print0 | xargs -0 update-nix-fetchgit) &
+
+wait
 
 git diff --exit-code || {
     nix --experimental-features "nix-command flakes" flake check

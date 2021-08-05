86326b8c3c01605b7dd23a25430309bd65bf445d
Michael Adler <therisen06@gmail.com>
2021-08-05 07:51:44 +0200

    brave, firefox: only download deb if update available

--- pkgs/brave/update.sh
+++ pkgs/brave/update.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -p curl -p bash -p nix-prefetch -i bash
+#!nix-shell -p curl -p bash -p nix-prefetch -p jq -i bash
 # shellcheck shell=bash
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-set -eux
+set -eu
 
 version="$(curl -sL https://brave-browser-apt-release.s3.brave.com/dists/stable/main/binary-amd64/Packages | sed -r -n 's/^Version: (.*)/\1/p' | head -n1)"
-url=$(printf "https://github.com/brave/brave-browser/releases/download/v%s/brave-browser_%s_amd64.deb" "$version" "$version")
-sha256=$(nix-prefetch-url "$url")
+old_version=$(jq -r '.version' <$SCRIPT_DIR/metadata.json)
 
-printf '{"version": "%s", "url": "%s", "sha256": "%s"}\n' "$version" "$url" "$sha256" >"$SCRIPT_DIR/metadata.json"
+if [ ! "$old_version" = "$version" ]; then
+    echo "Updating from $old_version to $version ..."
+    url=$(printf "https://github.com/brave/brave-browser/releases/download/v%s/brave-browser_%s_amd64.deb" "$version" "$version")
+    sha256=$(nix-prefetch-url "$url")
+    printf '{"version": "%s", "url": "%s", "sha256": "%s"}\n' "$version" "$url" "$sha256" >"$SCRIPT_DIR/metadata.json"
+else
+    echo "No update available"
+fi
+
--- pkgs/firefox-bin-unwrapped/update.sh
+++ pkgs/firefox-bin-unwrapped/update.sh
@@ -2,10 +2,16 @@
 #!nix-shell -p curl -p jq -p bash -p nix-prefetch -i bash
 # shellcheck shell=bash
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-set -eux
+set -eu
 
 FF_VERSION=$(curl -Ls https://product-details.mozilla.org/1.0/firefox.json | grep '"version":' | awk -F: '{print $2;}' | sort -V | grep -E -v '.*b[0-9]+"$' | tail -n1 | tr -d '" ')
-FF_URL="https://archive.mozilla.org/pub/firefox/releases/${FF_VERSION}/linux-x86_64/en-US/firefox-${FF_VERSION}.tar.bz2"
-FF_SHA256=$(nix-prefetch-url "$FF_URL")
+FF_VERSION_OLD=$(jq -r '.version' <$SCRIPT_DIR/metadata.json)
 
-printf '{"version": "%s", "url": "%s", "sha256": "%s"}\n' "$FF_VERSION" "$FF_URL" "$FF_SHA256" >"$SCRIPT_DIR/metadata.json"
+if [ ! "$FF_VERSION_OLD" = "$FF_VERSION" ]; then
+    echo "Updating from $old_version to $version ..."
+    FF_URL="https://archive.mozilla.org/pub/firefox/releases/${FF_VERSION}/linux-x86_64/en-US/firefox-${FF_VERSION}.tar.bz2"
+    FF_SHA256=$(nix-prefetch-url "$FF_URL")
+    printf '{"version": "%s", "url": "%s", "sha256": "%s"}\n' "$FF_VERSION" "$FF_URL" "$FF_SHA256" >"$SCRIPT_DIR/metadata.json"
+else
+    echo "No update available"
+fi

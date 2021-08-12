6bdbc654fb8d938931e7066e1f324c4074f5a4de
Michael Adler <therisen06@gmail.com>
2021-08-12 10:58:37 +0200

    brave: fix is now upstream

--- pkgs/brave/default.nix
+++ pkgs/brave/default.nix
@@ -6,14 +6,6 @@ prev.brave.overrideAttrs (old: {
 
   version = metadata.version;
 
-  postInstall = ''
-    for fname in brave crashpad_handler; do
-        patchelf \
-            --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-            --set-rpath "$(patchelf --print-rpath $out/opt/brave.com/brave/brave)" $out/opt/brave.com/brave/$fname
-    done
-  '';
-
   src = prev.fetchurl {
     inherit (metadata) url sha256;
   };

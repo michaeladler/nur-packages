f02980d7da07877b9ae6a5ff347961ddbcb6911c
Michael Adler <therisen06@gmail.com>
2021-08-18 07:40:34 +0200

    Fix firefox update script

--- pkgs/firefox-bin-unwrapped/update.sh
+++ pkgs/firefox-bin-unwrapped/update.sh
@@ -5,10 +5,10 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 set -eu
 
 FF_VERSION=$(curl -Ls https://product-details.mozilla.org/1.0/firefox.json | grep '"version":' | awk -F: '{print $2;}' | sort -V | grep -E -v '.*b[0-9]+"$' | tail -n1 | tr -d '" ')
-FF_VERSION_OLD=$(jq -r '.version' <$SCRIPT_DIR/metadata.json)
+FF_VERSION_OLD=$(jq -r '.version' <"$SCRIPT_DIR"/metadata.json)
 
 if [ ! "$FF_VERSION_OLD" = "$FF_VERSION" ]; then
-    echo "Updating from $old_version to $version ..."
+    echo "Updating from $FF_VERSION_OLD to $FF_VERSION ..."
     FF_URL="https://archive.mozilla.org/pub/firefox/releases/${FF_VERSION}/linux-x86_64/en-US/firefox-${FF_VERSION}.tar.bz2"
     FF_SHA256=$(nix-prefetch-url "$FF_URL")
     printf '{"version": "%s", "url": "%s", "sha256": "%s"}\n' "$FF_VERSION" "$FF_URL" "$FF_SHA256" >"$SCRIPT_DIR/metadata.json"

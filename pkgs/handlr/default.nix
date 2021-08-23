e0be04c638a6c05ade8d4f404bec57c40d25577f
Michael Adler <therisen06@gmail.com>
2021-08-23 08:05:24 +0200

    handlr patch has been merged upstream

--- pkgs/handlr/default.nix
+++ pkgs/handlr/default.nix
@@ -2,8 +2,12 @@ final: prev:
 
 prev.handlr.overrideAttrs (old: {
 
-  patches = (old.patches or [ ]) ++ [
-    ./patches/0001-Fix-crash-on-read-only-mimeapps.list.patch
-  ];
+  version = "unstable-2021-07-30";
+  src = prev.fetchFromGitHub {
+    owner = "chmln";
+    repo = "handlr";
+    rev = "b9b71369f855eb48f4ecca051515feb05d68f87d";
+    sha256 = "0kkgjzxrny7vhv7xhglr4m2k69yq2salvjfiqxgqkjiczbrsr95l";
+  };
 
 })

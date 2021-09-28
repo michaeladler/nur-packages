136713b1b507c64b071d4e70e67b3c2277e16564
Michael Adler <therisen06@gmail.com>
2021-09-28 16:08:02 +0200

    brave: simplify update.sh

--- pkgs/brave/update.sh
+++ pkgs/brave/update.sh
@@ -4,15 +4,15 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 set -eu
 
-version="$(curl -sL https://brave-browser-apt-release.s3.brave.com/dists/stable/main/binary-amd64/Packages | sed -r -n 's/^Version: (.*)/\1/p' | head -n1)"
-old_version=$(jq -r '.version' <$SCRIPT_DIR/metadata.json)
+version="$(curl -sL https://brave-browser-downloads.s3.brave.com/latest/release.version)"
+old_version=$(jq -r '.version' <"$SCRIPT_DIR"/metadata.json)
 
-if [ ! "$old_version" = "$version" ]; then
-    echo "Updating from $old_version to $version ..."
-    url=$(printf "https://github.com/brave/brave-browser/releases/download/v%s/brave-browser_%s_amd64.deb" "$version" "$version")
-    sha256=$(nix-prefetch-url "$url")
-    printf '{"version": "%s", "url": "%s", "sha256": "%s"}\n' "$version" "$url" "$sha256" >"$SCRIPT_DIR/metadata.json"
-else
-    echo "No update available"
+if [ "$old_version" = "$version" ]; then
+    echo "brave: no update available"
+    exit 0
 fi
 
+echo "brave: $old_version -> $version"
+url=$(printf "https://github.com/brave/brave-browser/releases/download/v%s/brave-browser_%s_amd64.deb" "$version" "$version")
+sha256=$(nix-prefetch-url "$url")
+printf '{"version": "%s", "url": "%s", "sha256": "%s"}\n' "$version" "$url" "$sha256" >"$SCRIPT_DIR/metadata.json"

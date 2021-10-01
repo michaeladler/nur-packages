d51065a3273c817f0740e5d873cec578bcbb7830
Michael Adler <therisen06@gmail.com>
2021-10-01 22:56:13 +0200

    pretty json

--- pkgs/brave/update.sh
+++ pkgs/brave/update.sh
@@ -5,7 +5,11 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 set -eu
 
 version="$(curl -sL https://brave-browser-downloads.s3.brave.com/latest/release.version)"
-old_version=$(jq -r '.version' <"$SCRIPT_DIR"/metadata.json)
+if [ -e "$SCRIPT_DIR"/metadata.json ]; then
+    old_version=$(jq -r '.version' <"$SCRIPT_DIR"/metadata.json)
+else
+    old_version=
+fi
 
 if [ "$old_version" = "$version" ]; then
     echo "brave: no update available"
@@ -15,4 +19,4 @@ fi
 echo "brave: $old_version -> $version"
 url=$(printf "https://github.com/brave/brave-browser/releases/download/v%s/brave-browser_%s_amd64.deb" "$version" "$version")
 sha256=$(nix-prefetch-url "$url")
-printf '{"version": "%s", "url": "%s", "sha256": "%s"}\n' "$version" "$url" "$sha256" >"$SCRIPT_DIR/metadata.json"
+printf '{"version": "%s", "url": "%s", "sha256": "%s"}\n' "$version" "$url" "$sha256" | jq >"$SCRIPT_DIR/metadata.json"

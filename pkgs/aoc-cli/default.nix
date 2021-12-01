8d2337662769601d224c4c51d933752324ff20c8
Michael Adler <therisen06@gmail.com>
2021-12-01 07:19:37 +0100

    update aoc cli

--- pkgs/aoc-cli/default.nix
+++ pkgs/aoc-cli/default.nix
@@ -2,20 +2,20 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "aoc-cli";
-  version = "unstable-2020-12-10";
+  version = "unstable-2021-11-30";
 
   src = fetchFromGitHub {
     owner = "scarvalhojr";
     repo = "aoc-cli";
-    rev = "9e8b2c44e18f5493f224c04b58cd367936d8e40d";
-    sha256 = "1p1qsck5k7hn2j8zqgnj154c3qjjibbgf8xks43ycbizisfdhlf8";
+    rev = "c468277d54c7fe0d79da25a9266d8c27fd7d959f";
+    sha256 = "1kyapf0dshkl4prl15xw0v6lkpk6kxwpqpwlqbv1xm93k1zwk7xm";
   };
 
   nativeBuildInputs = [ pkg-config ];
 
   buildInputs = [ openssl ];
 
-  cargoSha256 = "sha256-qZaGRgTaEGWja+YVh0woGmz+u+rB8+UtSiKs1FttlEQ=";
+  cargoSha256 = "sha256-axx89KPr3SJPx0ZIQu8aNsNha97pPkfhd4g40p3cOZE=";
 
   meta = with lib; {
     description = "Advent of Code command-line helper tool";

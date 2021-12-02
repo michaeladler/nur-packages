c3107cff9a7007bc37159eece3d4a211ff30d229
Michael Adler <therisen06@gmail.com>
2021-12-02 08:16:22 +0100

    i3-gaps

--- pkgs/i3-gaps/default.nix
+++ pkgs/i3-gaps/default.nix
@@ -1,13 +1,15 @@
 final: prev:
 
 prev.i3-gaps.overrideAttrs (old: {
-  version = "unstable-2021-11-27";
+  version = "unstable-2021-12-01";
+
+  buildInputs = old.buildInputs ++ [ prev.pcre2 ];
 
   src = prev.fetchFromGitHub {
     owner = "Airblader";
     repo = "i3";
-    rev = "6aaae79c8665f6dba8ff8b4c6263140d1a618fc7"; # gaps-next
-    sha256 = "08nw3dn46dir1czwd6hw6slrz4lh9gvbv9ynaqa2vkfs742jbrgx";
+    rev = "7af1bb4d1d216ec01fa451bf8e0d8ba78eefd0dc"; # gaps-next
+    sha256 = "16p5f8is6f0x3p0ahhwk8q5vgc5gv4c0gai26ydgg2q02ksrlklf";
   };
 
 })

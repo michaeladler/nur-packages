04e5f9b3492ce8bce919454e6f5781bfce604f46
Michael Adler <therisen06@gmail.com>
2022-01-21 14:23:44 +0100

    efibootguard with shell completion

--- pkgs/efibootguard/default.nix
+++ pkgs/efibootguard/default.nix
@@ -9,21 +9,23 @@
 , glibc
 , pciutils
 , python3
+, python3Packages
+, installShellFiles
 }:
 
 stdenv.mkDerivation rec {
 
   pname = "efibootguard";
-  version = "unstable-2021-11-10";
+  version = "unstable-2021-12-12";
 
   src = fetchFromGitHub {
     owner = "siemens";
     repo = "efibootguard";
-    rev = "e499d403d6049119a4e75c6145678ce9d3e82b64"; # pin
-    sha256 = "13y3c8b8jsff80k9cdwx23i1gv3b5nmvbm9w86qnf343pnr6ddp5";
+    rev = "798491274ddfcd1b290dc808609125a49ca5c451";
+    sha256 = "02gyr55nis79ch5flcs4cljzvl0hx3y18clnkwc5fg0j83y1v1r3";
   };
 
-  nativeBuildInputs = [ autoreconfHook pkgconfig check python3 ];
+  nativeBuildInputs = [ autoreconfHook pkgconfig check (python3.withPackages (ps: with ps; [ shtab ])) installShellFiles ];
 
   buildInputs = [ gnu-efi zlib glibc pciutils ];
 
@@ -31,6 +33,16 @@ stdenv.mkDerivation rec {
     "--with-gnuefi-include-dir=${gnu-efi}/include/efi"
     "--with-gnuefi-lib-dir=${gnu-efi}/lib"
   ];
+
+  postInstall = ''
+    installShellCompletion --zsh --name _bg_printenv $out/share/efibootguard/completion/zsh/_bg_printenv
+    installShellCompletion --zsh --name _bg_setenv $out/share/efibootguard/completion/zsh/_bg_setenv
+
+    installShellCompletion --bash --name bg_printenv.bash $out/share/efibootguard/completion/bash/bg_printenv.bash
+    installShellCompletion --bash --name bg_setenv.bash $out/share/efibootguard/completion/bash/bg_setenv.bash
+
+    rm -rf $out/share/efibootguard/completion
+  '';
 
   meta = with lib; {
     homepage = "https://github.com/siemens/efibootguard";

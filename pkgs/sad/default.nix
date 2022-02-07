3451d18a3ced28ec24944a3093992ba73f3f0831
Michael Adler <therisen06@gmail.com>
2022-02-07 08:02:17 +0100

    pin sad

--- pkgs/sad/default.nix
+++ pkgs/sad/default.nix
@@ -10,7 +10,7 @@ naersk-lib.buildPackage rec {
   src = fetchFromGitHub {
     owner = "ms-jpq";
     repo = "sad";
-    rev = "03c6761e94e392b1c3d8a36eedc9289c0bc4226a";
+    rev = "03c6761e94e392b1c3d8a36eedc9289c0bc4226a"; # pin
     sha256 = "1kdiqlkpqw1ap1j8ckv1wqxqw3wdq84mkyj694slxnbn7x21ccwy";
   };
 

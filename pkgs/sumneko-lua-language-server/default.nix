d37b98d10d25fca2e4124d936cb8835641f21cee
Michael Adler <therisen06@gmail.com>
2022-04-26 09:25:19 +0200

    sumneko-lua-language-server: downgrade to 2.X
    3.X contains too many breaking changes for now

--- pkgs/sumneko-lua-language-server/default.nix
+++ pkgs/sumneko-lua-language-server/default.nix
@@ -1,13 +1,13 @@
 final: prev:
 
 prev.sumneko-lua-language-server.overrideAttrs (old: {
-  version = "unstable-2022-04-25";
+  version = "unstable-2022-04-09";
 
   src = prev.fetchFromGitHub {
     owner = "sumneko";
     repo = "lua-language-server";
-    rev = "14bf8a0d00cd6e485eff604c67dc21e29b80eef1"; # tags/*
-    sha256 = "0x1la1005q3da4gzf5iywb69xc4r85w89v10pqjnfnjkbjnxw5xg";
+    rev = "4d0e01ce2699e0fecd14563c42ad56131b89cf56"; # tags/2*
+    sha256 = "1dbfpjkpa03bq8yxixml7wcd9drw3g5jfrxr33qycn905w299qvh";
     fetchSubmodules = true;
   };
 

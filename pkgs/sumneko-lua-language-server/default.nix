be0bde2fbc194d89caaa57f014000ad0415611e9
Michael Adler <therisen06@gmail.com>
2022-05-16 15:47:53 +0200

    sumneko

--- pkgs/sumneko-lua-language-server/default.nix
+++ pkgs/sumneko-lua-language-server/default.nix
@@ -1,13 +1,13 @@
 final: prev:
 
 prev.sumneko-lua-language-server.overrideAttrs (old: {
-  version = "unstable-2022-04-09";
+  version = "unstable-2022-05-16";
 
   src = prev.fetchFromGitHub {
     owner = "sumneko";
     repo = "lua-language-server";
-    rev = "4d0e01ce2699e0fecd14563c42ad56131b89cf56"; # tags/2*
-    sha256 = "1dbfpjkpa03bq8yxixml7wcd9drw3g5jfrxr33qycn905w299qvh";
+    rev = "e62d964ff57cc0b37eb908315f9afe3ce6a213d7"; # tags/*
+    sha256 = "1jkgr1jjfcb6sm8cvcs10y20dn2hjd9y96c3m30gbyl191d0z7lz";
     fetchSubmodules = true;
   };
 

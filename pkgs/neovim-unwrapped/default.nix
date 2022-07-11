1f4f7e3a9aa204f3dcf5bbef2b8c385fb984ff59
Michael Adler <therisen06@gmail.com>
2022-07-11 08:04:15 +0200

    update neovim

--- pkgs/neovim-unwrapped/default.nix
+++ pkgs/neovim-unwrapped/default.nix
@@ -1,13 +1,13 @@
 final: prev:
 
 prev.neovim-unwrapped.overrideAttrs (old: {
-  version = "unstable-2022-06-30";
+  version = "unstable-2022-07-10";
 
   src = prev.fetchFromGitHub {
     owner = "neovim";
     repo = "neovim";
-    rev = "ba5be650a796f868f1caf822370e694d24600666";
-    sha256 = "sha256-pa9aKMUt5IybeAyskz9UPakDohqspDX1O54A7/k1Lt8=";
+    rev = "95c65a6b221fe6e1cf91e8322e7d7571dc511a71";
+    sha256 = "1h4mxgk5mv0qh55zjmyg8xk98g8rw7hzflfg1xw7dnhdiczmyyxn";
     fetchSubmodules = true;
   };
 

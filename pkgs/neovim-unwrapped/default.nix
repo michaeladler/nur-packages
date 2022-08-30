db51942e85605c88fb93e69a4b12bc0964a3f090
Michael Adler <therisen06@gmail.com>
2022-08-30 09:13:00 +0200

    nvim: downgrade

--- pkgs/neovim-unwrapped/default.nix
+++ pkgs/neovim-unwrapped/default.nix
@@ -1,13 +1,13 @@
 final: prev:
 
 prev.neovim-unwrapped.overrideAttrs (old: {
-  version = "unstable-2022-08-30";
+  version = "unstable-2022-08-29";
 
   src = prev.fetchFromGitHub {
     owner = "neovim";
     repo = "neovim";
-    rev = "568737d5b39a4b58cab05d4edc2599653979770c";
-    sha256 = "058k9pivl451gqw6nac1pjc75wprgyky3n7klxw9gapv4kmyxj84";
+    rev = "efacb6e974fa6391bcc916749103f04fa9b9f6f7";
+    sha256 = "1fnv0brbk4rghc6hivyyxcniccfvx0vn19bw6w20ybmn40k9jwsn";
     fetchSubmodules = true;
   };
 

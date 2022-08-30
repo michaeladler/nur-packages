8326449999314f272916d6b9671392a0e256fee4
Michael Adler <therisen06@gmail.com>
2022-08-30 11:28:52 +0200

    downgrade neovim

--- pkgs/neovim-unwrapped/default.nix
+++ pkgs/neovim-unwrapped/default.nix
@@ -1,13 +1,13 @@
 final: prev:
 
 prev.neovim-unwrapped.overrideAttrs (old: {
-  version = "unstable-2022-06-26";
+  version = "unstable-2022-08-28";
 
   src = prev.fetchFromGitHub {
     owner = "neovim";
     repo = "neovim";
-    rev = "e8ee6733926db83ef216497a1d660a173184ff39"; # tags/v*
-    sha256 = "1jn4i4ral79ys06i50bimylm515sfh41z503gj50a74h1ylg0z4w";
+    rev = "7a9b5937966d3db852c84b8eb232e77d92d3c355"; # pin
+    sha256 = "1x9lvhyhs6zphv0k0mvnv5rnhijss2zasaim4b18k1y9biqb8g3b";
     fetchSubmodules = true;
   };
 

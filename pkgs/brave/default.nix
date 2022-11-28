dd39ada9389dc3dbcc8ad05136ff3f99574861b0
Michael Adler <therisen06@gmail.com>
2022-11-28 09:44:18 +0100

    brave: kerberos support

--- pkgs/brave/default.nix
+++ pkgs/brave/default.nix
@@ -31,6 +31,7 @@
 , libXrender
 , libXScrnSaver
 , libxshmfence
+, libkrb5
 , libXtst
 , mesa
 , nspr
@@ -78,6 +79,7 @@ rpath = lib.makeLibraryPath [
   libXrandr
   libXrender
   libxshmfence
+  libkrb5
   libXtst
   libuuid
   mesa

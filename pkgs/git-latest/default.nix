01dd193fa24b27c686d068748e57286e68dee9ad
Michael Adler <therisen06@gmail.com>
2023-01-18 10:27:37 +0100

    update git

--- pkgs/git-latest/default.nix
+++ pkgs/git-latest/default.nix
@@ -1,11 +1,11 @@
 final: prev:
 
 prev.git.overrideAttrs (old: rec {
-  version = "2.39.0";
+  version = "2.39.1";
 
   src = final.fetchurl {
     url = "https://www.kernel.org/pub/software/scm/git/git-${version}.tar.xz";
-    sha256 = "0nr6d46z3zfxbr1psww7vylva3mw6vbhnywixhywm6aszc9rn6ds";
+    sha256 = "0qf1wly7zagg23svpv533va5v213y7y3lfw76ldkf35k8w48m8s0";
   };
 
 })

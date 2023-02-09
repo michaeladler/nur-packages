4074972390cf7d6b03b825d4fd1ff57df62dd32b
Michael Adler <therisen06@gmail.com>
2023-02-09 09:53:08 +0100

    chromium-floccus json

--- pkgs/chromium-floccus/default.nix
+++ pkgs/chromium-floccus/default.nix
@@ -10,12 +10,21 @@ stdenv.mkDerivation rec {
     inherit (metadata) url sha256;
   };
 
+  nativeBuildInputs = with pkgs; [ go-crx3 ];
+
   unpackPhase = "true";
   buildPhase = "true";
 
   installPhase = ''
     mkdir -p $out/share/${pname}
-    cp $src $out/share/${pname}/${pname}.crx
+    CRX_TARGET=$out/share/${pname}/${pname}.crx
+    cp $src $CRX_TARGET
+    cat <<EOF > $out/share/${pname}/$(crx3 id $CRX_TARGET).json
+    {
+      "external_crx": "$CRX_TARGET",
+      "external_version": "${version}"
+    }
+    EOF
   '';
 
   meta = with lib; {

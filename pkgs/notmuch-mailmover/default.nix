40750cc78c743c83d543f6f8aecca3837c4b41c0
Michael Adler <therisen06@gmail.com>
2023-08-01 09:35:45 +0200

    notmuch-mailmover: updates

--- pkgs/notmuch-mailmover/default.nix
+++ pkgs/notmuch-mailmover/default.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "notmuch-mailmover";
-  version = "unstable-2023-07-03";
+  version = "unstable-2023-08-01";
 
   src = fetchFromGitHub {
     owner = "michaeladler";
     repo = "notmuch-mailmover";
-    rev = "94e0064af4eccc7f6002883e8b6efbc59365bb2c";
-    sha256 = "0x5qzyqgqjv9s1jn9661ycn3cgraiz8xbjpvvypdbk5172dvg932";
+    rev = "84467c7952ab997eea75bf7366e0fa48d061a5a9";
+    sha256 = "0ffgn4i5ksq0g1agvfvc20njr424p2mk57qpkp7sif8gs28cxx2h";
   };
 
-  cargoHash = "sha256-mu6jSJ15YTVD1Ju7CUTxygACj2I+gC3fuYe/z9Sm49g=";
+  cargoHash = "sha256-WBUU/yX0M44HHfOdfYaCN+DdBBDWQ3p5qroFfr7GpPA=";
 
   buildInputs = [ notmuch ];
 

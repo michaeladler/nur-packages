e1cfeac7242f930f25a944f5e0e6f7fd945136a3
Michael Adler <therisen06@gmail.com>
2023-10-23 14:07:37 +0200

    chore: update notmuch-mailmover

--- pkgs/notmuch-mailmover/default.nix
+++ pkgs/notmuch-mailmover/default.nix
@@ -2,22 +2,33 @@
 , rustPlatform
 , fetchFromGitHub
 , notmuch
+, installShellFiles
 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "notmuch-mailmover";
-  version = "unstable-2023-10-20";
+  version = "unstable-2023-10-22";
 
   src = fetchFromGitHub {
     owner = "michaeladler";
     repo = "notmuch-mailmover";
-    rev = "5df1746d3298eb176f8e80ce270046fca6450edf";
-    sha256 = "1fw3n3vb7hlvvyy1ghpfb6zh1sdr54hdgccrsyssgyk90s7pbhzg";
+    rev = "6d04e455c3224b383dd92f6ddbd2743efbdd270f";
+    sha256 = "134jn09iy39p683nd8j1hwniqnbws5c7r3v8i33wzb6g4zqhi3v8";
   };
 
-  cargoHash = "sha256-wWDFqF0aBDsMfy8uYFs1CTn4MhfGdmb89sdgl0b6n2o=";
+  cargoHash = "sha256-lSK+RLq7A8K39Dl+a+0pRZ7yuUDmK5B37aYC82wTRt4=";
+
+  nativeBuildInputs = [ installShellFiles ];
 
   buildInputs = [ notmuch ];
+
+  postInstall = ''
+    installManPage share/notmuch-mailmover.1
+    installShellCompletion --cmd notmuch-mailmover \
+      --bash share/notmuch-mailmover.bash \
+      --fish share/notmuch-mailmover.fish \
+      --zsh share/_notmuch-mailmover
+  '';
 
   meta = with lib; {
     description = "notmuch-mailmover is a CLI application to assign notmuch *tagged* mails to IMAP folders";

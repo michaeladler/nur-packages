5515436842b8ba2eabf3bde0c1f0084386965ebf
Michael Adler <michael.adler@siemens.com>
2024-08-22 09:30:15 +0200

    remove some packages

--- overlays/common.nix
+++ overlays/common.nix
@@ -11,26 +11,19 @@ in
 
   # missing upstream
   asciinema-edit = callPackage ../pkgs/asciinema-edit { };
-  atool2 = callPackage ../pkgs/atool2 { };
   bed = callPackage ../pkgs/bed { };
-  btrfs-du = callPackage ../pkgs/btrfs-du { };
   btrfs-list = callPackage ../pkgs/btrfs-list { };
   cinclude2dot = callPackage ../pkgs/cinclude2dot { };
-  clang-format-diff = callPackage ../pkgs/clang-format-diff { };
   colorized-logs = callPackage ../pkgs/colorized-logs { };
   demo-magic = callPackage ../pkgs/demo-magic { };
   efibootguard = callPackage ../pkgs/efibootguard { };
   extension-downloader = callPackage ../pkgs/extension-downloader { };
   fmtlog = callPackage ../pkgs/fmtlog { };
-  fzy-lua-native = callPackage ../pkgs/fzy-lua-native { };
-  git-buildpackage = callPackage ../pkgs/git-buildpackage { };
-  go-crx3 = callPackage ../pkgs/go-crx3 { };
   go-mod-outdated = callPackage ../pkgs/go-mod-outdated { };
   kas-container = callPackage ../pkgs/kas-container { };
   libubootenv = callPackage ../pkgs/libubootenv { };
   luaprompt = callPackage ../pkgs/luaprompt { };
   notmuch-mailmover = callPackage ../pkgs/notmuch-mailmover { };
-  poop = callPackage ../pkgs/poop { };
   river-bsp-layout = callPackage ../pkgs/river-bsp-layout { };
   skipcpio = callPackage ../pkgs/skipcpio { };
   vcalendar-filter = callPackage ../pkgs/vcalendar-filter { };

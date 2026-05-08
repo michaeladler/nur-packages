36a6bdf89f02a48c068536cdb39cc2f5bbb5f7cd
Michael Adler <therisen06@gmail.com>
2026-05-08 08:16:08 +0200

    tokscale: init

---
+++ pkgs/tokscale/default.nix
@@ -0,0 +1,57 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  pkg-config,
+  openssl,
+  sqlite,
+  nix-update-script,
+}:
+
+rustPlatform.buildRustPackage {
+  pname = "tokscale";
+  version = "0-unstable-2026-05-05";
+
+  src = fetchFromGitHub {
+    owner = "junhoyeo";
+    repo = "tokscale";
+    rev = "3a9045bc1b9aa245ca44ffcdca68bdaaa09520eb";
+    hash = "sha256-XX5r5ih71xnRA1D3oLKWbH6aHXo9eODKLzteCX2+gyk=";
+  };
+
+  cargoHash = "sha256-1MsRjuyvSxLo8dFK3yH5i+jeeKFBVGX/4excFYImBc4=";
+
+  nativeBuildInputs = [
+    pkg-config
+  ];
+
+  buildInputs = [
+    openssl
+    sqlite
+  ];
+
+  env = {
+    OPENSSL_NO_VENDOR = true;
+  };
+
+  checkFlags = [
+    # Tries to make network requests to other hosts
+    "--skip=test_graph_single_day_filter_uses_local_timezone_boundaries"
+    "--skip=test_pricing_command_json"
+    "--skip=test_pricing_command_success"
+    "--skip=test_pricing_command_with_provider"
+  ];
+
+  doInstallCheck = true;
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "CLI tool for tracking token usage from various agentic coding tools like Claude Code and OpenCode etc.";
+    downloadPage = "https://github.com/junhoyeo/tokscale";
+    homepage = "https://tokscale.ai";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ kpbaks ];
+    mainProgram = "tokscale";
+  };
+}

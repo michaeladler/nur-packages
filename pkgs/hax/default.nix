e02fc96d6c03ea8d19b4390a623dac9f2048a802
Michael Adler <therisen06@gmail.com>
2026-08-06 16:23:19 +0200

    hax: init

---
+++ pkgs/hax/default.nix
@@ -0,0 +1,44 @@
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  meson,
+  ninja,
+  pkgconf,
+  curl,
+  jansson,
+  diffutils,
+
+}:
+
+stdenv.mkDerivation {
+  pname = "hax";
+  version = "unstable-2026-08-06";
+
+  src = fetchFromGitHub {
+    owner = "OleksandrChekhovskyi";
+    repo = "hax";
+    rev = "cb1a429bd5b2a5908ac7f5b84159d0220d95d7b7";
+    sha256 = "014mrl3cg2102r4y67j93jphzk22msmxplv0ab126pnpddbmky62";
+  };
+
+  nativeBuildInputs = [
+    pkgconf
+    meson
+    ninja
+  ];
+
+  buildInputs = [
+    curl
+    jansson
+    diffutils
+  ];
+
+  meta = with lib; {
+    homepage = "https://github.com/OleksandrChekhovskyi/hax";
+    description = "A minimalist, terminal-native coding agent written in C.";
+    maintainers = [ maintainers.michaeladler ];
+    platforms = platforms.unix;
+    license = licenses.mit;
+  };
+}

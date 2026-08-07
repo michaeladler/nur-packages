20cb7934c317805b6b56e528197af1c5eb3c8690
Michael Adler <therisen06@gmail.com>
2026-08-07 11:49:15 +0200

    landstrip: init

---
+++ pkgs/landstrip/default.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+}:
+
+rustPlatform.buildRustPackage {
+  pname = "landstrip";
+  version = "unstable-2026-08-06";
+
+  src = fetchFromGitHub {
+    owner = "landstrip";
+    repo = "landstrip";
+    rev = "4830ac62d5b972c1a40334b7b1a68e7d69701c6a";
+    sha256 = "1q3dy5yv5nl3g8mwbr6rbya48578a7hy5h7h8d6459viclwhinn2";
+  };
+
+  cargoHash = "sha256-axM21y0HKtn2KOtXXXXdn8jv9vgWCn02snNxTnsf2g4=";
+
+  doCheck = false;
+
+  meta = with lib; {
+    description = "A sandbox for coding agents with parametrized state.";
+    homepage = "https://github.com/landstrip/landstrip";
+    license = licenses.lgpl21Plus;
+  };
+}
